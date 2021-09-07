b48a98657e42c9617108b7a5ed65a2cf1d16188a
Rafael Luiz <rafacoder2020@outlook.com>
2021-09-07 10:23:39 -0300

    Project Inicialization

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import Document, { Html, Main, NextScript, Head } from 'next/document'
+
+export default class MyDocument extends Document {
+  render(){
+    return (
+      <Html>
+        <Head/>
+       
+        <body>
+          <Main />
+
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
